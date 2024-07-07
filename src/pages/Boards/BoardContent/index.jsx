90147b976882ecc65b941204fe90b68756a5ebf4
chuyentran2511 <tchuyen23@gmail.com>
2024-07-07 18:58:30 +0800

    color

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -7,9 +7,25 @@ function Boardcontent() {
       width:'100%',
       height:(theme)=>`calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boarBarHeight})`,
       display:'flex',
-      alignItems:'center'
+      
     }}>
-      box content
+      <Box sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        bgcolor: ( theme ) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+        ml: 2,
+        borderRadius:'6px'
+      }}>
+        <Box sx={{ }}>
+          header
+        </Box>
+        <Box sx={{ }}>
+          list card
+        </Box>
+        <Box sx={{ }}>
+          foooter
+        </Box>
+      </Box>
     </Box>
   )
 }
